e9e392596c981685e226b239a5c9bfff00b625d1
danyakhairullin <khdaniilnice@gmail.com>
2026-01-07 09:56:23 +0700

    Написал задание

--- HomeWork/ex_1_two_sum.py
+++ HomeWork/ex_1_two_sum.py
@@ -1,7 +1,11 @@
+# Дан список чисел nums и целое чилсо target. Вернуть индекс двух чисел сумма которых равна target.
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        slovar = {}
-        for i, n in enumerate(nums):
+        # Создаётся метод, который получает список nums и число target
+        # Должны получить от этого метода тоже список
+        slovar = {} # Создаём словарь
+        for i, n in enumerate(nums): # Используем цикл от i(индекс) и n(значения) в списке nums
+            # функция enumerate возвращает i(индекс) и n(значения) списка
             num = target - n
             if num in slovar:
                 return [slovar[num],i]
